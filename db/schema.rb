15738be02cfaaf27a605cc2493a5e1c05d9ee443
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2014-11-16 17:02:39 -0700

    Stuff for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141116233429) do
+ActiveRecord::Schema.define(version: 20141116235503) do
 
   create_table "images", force: true do |t|
     t.integer  "user_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20141116233429) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    
+
   end
 
 end
