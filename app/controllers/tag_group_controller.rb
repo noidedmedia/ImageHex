06cc65395b883c306bfb3275a181722df87c69ab
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2014-11-21 10:37:03 -0700

    Tag Group controller plus some tests

---
+++ app/controllers/tag_group_controller.rb
@@ -0,0 +1,2 @@
+class TagGroupController < ApplicationController
+end
---
+++ spec/controllers/tag_group_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe TagGroupController do
+  include Devise::TestHelpers
+  context "when logged in" do
+    let(:image){FactoryGirl.create(:image)}
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      sign_in @user
+    end
+    describe 'get #new' do
+      it "responds successfully" do
+        get :new, image_id: image
+        expect(response).to be_success
+      end
+    end
+    describe "get #edit" do
+      it "responds successfully" do
+        get :edit, image_id: image
+        expect(response).to be_success
+      end
+      it "sets the variables correctly" do
+        group = FactoryGirl.create(:tag_group)
+        get :edit, image_id: image, id: group
+        expect(assigns(:tag_group)).to eq(group)
+      end
+    end
+  end
+  context "when not logged in" do
+    let(:image){FactoryGirl.create(:image)}
+    describe "get #new" do
+      it "redirects to the login page" do
+        get :new, image_id: image
+        expect(response).to redirect_to("users/sign_in")
+      end
+    end
+    describe "get #edit" do
+      it "redirects to the login page" do
+        get :new, image_id: image
+        expect(response).to redirect_to("users/sign_in")
+      end
+    end
+  end
+end
