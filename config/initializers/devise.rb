a70a80278e4438aca1a437ee18b718c1a9586281
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2014-11-27 20:15:41 -0700

    fix typo

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'admin@imagehex.com'
-  congif.mailer = "UserMailer"
+  config.mailer = "UserMailer"
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
