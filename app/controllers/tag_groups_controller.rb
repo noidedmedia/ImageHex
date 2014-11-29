7a8d825653f7a6ea8b7d14d657420a0a6921df99
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2014-11-29 16:48:46 -0700

    Some code for the tag_group model

--- app/controllers/tag_groups_controller.rb
+++ app/controllers/tag_groups_controller.rb
@@ -1,2 +1,26 @@
 class TagGroupsController < ApplicationController
+  def new
+    @tag_group = TagGroup.new
+  end
+
+  def create
+    @tag_group = TagGroup.create(tag_group_params)
+    if @tag_group.save
+      redirect_to image
+    else
+      render 'new'
+    end
+  end
+
+
+
+  protected
+  def tag_group_params
+    params.require(:tag_group)
+      .permit(:tag_group_string)
+      .merge(image_id: image.id) # Add in the image id automatically
+  end
+  def image
+    Image.find(params[:image_id])
+  end
 end
--- spec/models/tag_group_spec.rb
+++ spec/models/tag_group_spec.rb
@@ -5,4 +5,16 @@ describe TagGroup do
   it{should belong_to(:image)}
   it{should have_many(:tag_group_members)}
   it{should have_many(:tags).through(:tag_group_members)}
+  describe "The tag group string" do
+    it "seperates by commas" do
+      image = FactoryGirl.create(:image)
+      group = TagGroup.new
+      group.image = image
+      group.tag_group_string = "falling, dying, hitting things"
+      group.save
+      expect(group.tags.map(&:name)).to eq(["faling",
+                                            "dying",
+                                            "hitting things"])
+    end
+  end
 end
