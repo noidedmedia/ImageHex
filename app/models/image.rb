ac036739c88442aae9f9d46fdf18b5663ef27334
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2014-12-02 09:00:18 -0700

    Search backend partially done

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,20 +1,52 @@
 class Image < ActiveRecord::Base
-  ## 
-  # ASSOCIATIONS
+  ################
+  # ASSOCIATIONS #
+  ################
   has_attached_file :f, styles: { medium: "300x300>", thumb: "100x100>" }
   belongs_to :user
   has_many :tag_groups
   
-  ##
-  # ENUMS
+  #########
+  # ENUMS #
+  #########
+  
+  # What license the image is under
   enum license: [:public_domain, :all_rights_reserved, :cc_by, :cc_by_sa, :cc_by_nd, :cc_by_nc, :cc_by_nc_sa, :cc_by_nc_nd]
+  # What kind of image this is
   enum medium: [:photograph, :pencil, :paint, :digital_paint, :mixed_media, :three_dimensional_render]
 
-  ##
-  # VALIDATIONS
+  ###############
+  # VALIDATIONS #
+  ###############
   validates_attachment_content_type :f, content_type: /\Aimage\/.*\Z/
   validates_attachment_presence :f
   validates :user, presence: :true
   validates :license, presence: true
   validates :medium, presence: true
+
+  #################
+  # CLASS METHODS #
+  #################
+
+  
+  ##
+  # Take an ARRAY of COLLECTIONS of groups.
+  # Find all images which have at least one member from EACH COLLECTION
+  # This is done to allow people to search by two or more tag groups.
+  # So if they want to see an image where Donald Trump is wrestling
+  # with Vince McMahon, they can search "donald trump, wrestling" and
+  # "vine mcmahon, wrestling"
+  def self.from_groups(group)
+    # First, we fold the group over onto itself, selecting groups only if
+    # their image id is in the next group.
+    # Folds are a pretty high-level functional programming concept. Be sure
+    # to look up what it does if you don't get it.
+    group.inject do |l, r|
+      l.where(image_id: r.pluck(:image_id))
+    end
+    ##
+    # Now, the only groups which remain belong to images that match the
+    # query as a whole. Thus, we can simply grab the image_id.
+    self.where(id: group.pluck(:id))
+  end
 end
