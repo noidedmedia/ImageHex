a4b755d0a90b9f2c4e5a1bb5a8e598a06fbdddbd
connorshea <connor.james.shea@gmail.com>
2014-12-07 19:14:19 -0700

    Even more optimization! Crazy!

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,9 +3,6 @@ module DeviseHelper
     return "" if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t("errors.messages.not_saved",
-                      :count => resource.errors.count,
-                      :resource => resource.class.model_name.human.downcase)
 
     html = <<-HTML
     <div id="error_explanation">
