6e1211a098614fd402386a58d8cb8aa8fcfb03ae
connorshea <connor.james.shea@gmail.com>
2014-12-07 20:04:08 -0700

    I am stupid

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,19 +1,19 @@
 module DeviseHelper
   def devise_error_messages!
-    return "" if resource.errors.empty?
-
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-
+    return '' if resource.errors.empty?
+ 
+    messages = resource.errors.full_messages.map { |msg| content_tag(:p, msg) }.join
+    sentence = I18n.t('errors.messages.not_saved',
+      count: resource.errors.count,
+      resource: resource.class.model_name.human.downcase)
+ 
     html = <<-HTML
-    <div id="error_explanation">
-      <ul>#{messages}</ul>
+    <div class="alert alert-danger">
+      <p>#{sentence}</p>
+      #{messages}
     </div>
     HTML
-
+ 
     html.html_safe
-  end
-
-  def devise_error_messages?
-    resource.errors.empty? ? false : true
   end
 end
