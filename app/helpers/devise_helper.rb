b5f1d7e0b4a488611f8226db48ad5ba885915c32
connorshea <connor.james.shea@gmail.com>
2014-12-07 20:27:58 -0700

    I am a CSS Wizard

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,14 +2,10 @@ module DeviseHelper
   def devise_error_messages!
     return '' if resource.errors.empty?
  
-    messages = resource.errors.full_messages.map { |msg| content_tag(:p, msg) }.join(", ")
-    sentence = I18n.t('errors.messages.not_saved',
-      count: resource.errors.count,
-      resource: resource.class.model_name.human.downcase)
+    messages = resource.errors.full_messages.map { |msg| content_tag(:p, msg) }.join(', ')
  
     html = <<-HTML
     <div class="alert alert-danger">
-      <p>#{sentence}</p>
       #{messages}
     </div>
     HTML
