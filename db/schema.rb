7275ee3f241d6e0f6960408ff2d3185d66b721c9
connorshea <connor.james.shea@gmail.com>
2014-12-14 12:58:20 -0700

    Lots of comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208034705) do
+ActiveRecord::Schema.define(version: 20141214173427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "collection_images", force: true do |t|
+    t.integer  "collection_id"
+    t.integer  "image_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "collections", force: true do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "kind"
+  end
+
+  add_index "collections", ["user_id"], name: "index_collections_on_user_id", using: :btree
 
   create_table "images", force: true do |t|
     t.integer  "user_id"
