46f50c0e67f9f53ad9930c917b50bb3185874705
connorshea <connor.james.shea@gmail.com>
2014-12-23 18:25:08 -0700

    Precompiling assets

--- db/schema.rb
+++ db/schema.rb
@@ -16,29 +16,12 @@ ActiveRecord::Schema.define(version: 20141221012725) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "collection_images", force: :cascade do |t|
-    t.integer  "collection_id"
-    t.integer  "image_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "collections", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "name",       limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "kind"
-  end
-
-  add_index "collections", ["user_id"], name: "index_collections_on_user_id", using: :btree
-
   create_table "images", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "f_file_name",    limit: 255
-    t.string   "f_content_type", limit: 255
+    t.string   "f_file_name"
+    t.string   "f_content_type"
     t.integer  "f_file_size"
     t.datetime "f_updated_at"
     t.integer  "license"
@@ -49,9 +32,9 @@ ActiveRecord::Schema.define(version: 20141221012725) do
 
   create_table "reports", force: :cascade do |t|
     t.integer  "severity"
-    t.string   "message",         limit: 255
+    t.string   "message"
     t.integer  "reportable_id"
-    t.string   "reportable_type", limit: 255
+    t.string   "reportable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
@@ -78,31 +61,31 @@ ActiveRecord::Schema.define(version: 20141221012725) do
   add_index "tag_groups", ["image_id"], name: "index_tag_groups_on_image_id", using: :btree
 
   create_table "tags", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name",                   limit: 255
-    t.integer  "page_pref",                          default: 20
-    t.string   "confirmation_token",     limit: 255
+    t.string   "name"
+    t.integer  "page_pref",              default: 20
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
-    t.integer  "role",                               default: 0
+    t.string   "unconfirmed_email"
+    t.integer  "role",                   default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
