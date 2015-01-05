9a97bc29921717520c7936fccac3c58c5f43b3b4
connorshea <connor.james.shea@gmail.com>
2015-01-04 17:36:19 -0700

    This took at least an hour :(
    I hate JavaScript

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -5,15 +5,34 @@ function DropDown(el) {
 }
 
 DropDown.prototype = {
-  initEvents : function() {
+  initEvents: function() {
     var obj = this;
 
-    obj.dd.on('click', function(event){
+    obj.dd.on('click', function(event) {
       $(this).toggleClass('active');
+      clickOutside();
+    });
+
+    obj.opts.on('click',function(event){
+      event.stopPropagation();
     });
   }
 };
 
-$(window).ready(function() {
-  var dd = new DropDown( $('#dd') );
+function clickOutside() {
+  var classList = $('#dd').attr('class').split(/\s+/);
+  console.log(classList);
+
+  for (var i = 0; i < classList.length; i++) {
+    if (classList[i] == 'active') {
+      $('#dd').bind('clickoutside', function(event) {
+        $('#dd').unbind('clickoutside');
+        $('#dd').removeClass('active');
+      });
+    }
+  }
+}
+
+$(document).ready(function(){
+  var dd = new DropDown($('#dd'));
 });
