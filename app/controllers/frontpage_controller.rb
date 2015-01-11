e8045f8a3f65284a97a95e4dceb7942390d615b8
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-01-10 22:42:02 -0700

    Added ways to put a feed of images on the front page

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -1,5 +1,10 @@
 class FrontpageController < ApplicationController
   def index
-
+    if current_user
+      @images = current_user.image_feed
+      render "index_with_user"
+    else
+      render "index"
+    end
   end
 end
