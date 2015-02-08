8b194c3ffd667a23d9de3c7b144ef355c08ff052
connorshea <connor.james.shea@gmail.com>
2015-02-08 13:36:26 -0700

    Further improvements to the user page

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,17 @@
+function tabbedMenu() {  
+  $('ul#user-page-header-tabs li').click(function(){
+    var tabid = $(this).attr('data-tab');
+
+    $('ul#user-page-header-tabs li').removeClass('current');
+    $('.tab-content').removeClass('current');
+
+    $(this).addClass('current');
+    $('.tc-' + tabid).addClass('current');
+  });
+}
+
+var ready = function() {
+  tabbedMenu();
+};
+
+$(document).ready(ready);
