1789dc0163001310a7ec708c5a9ad279af8e9741
connorshea <connor.james.shea@gmail.com>
2015-02-15 19:29:19 -0700

    I DID IT

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,46 +1,54 @@
 function headerSearch() {
   var headersearch = $("#header-search");
   var headersearchinput = $("#header-search-input");
+  var windowwidth = $(window).width();
 
-  // Runs the function when the header-search input is the focused element.
-  headersearchinput.on('focus', function() {
-    
-    // Checks to make sure the "header-search" div doesn't already have an "active" class.
-    if (!headersearch.hasClass('active')) {
-      // Variable for moving up one in the HTML hierarchy.
-      var parentform = headersearchinput.parent('form');
+  // Only run if the browser window size doesn't imply a mobile device.
+  if (windowwidth >= 700) {
+    // Runs the function when the header-search input is the focused element.
+    headersearchinput.on('focus', function() {
+      
+      // Checks to make sure the "header-search" div doesn't already have an "active" class.
+      if (!headersearch.hasClass('active')) {
+        // Variable for moving up one in the HTML hierarchy.
+        var parentform = headersearchinput.parent('form');
 
-      // Moves up another level to the header-search div.
-      parentform.parent('#header-search').toggleClass('active');
-    }
+        // Moves up another level to the header-search div.
+        parentform.parent('#header-search').toggleClass('active');
+      }
 
-    // Binds any click outside the headersearch div to removing the "active" class
-    // and therefore hidings the header's search dropdown.
-    headersearch.bind('clickoutside', function(event){
-      $(this).toggleClass('active');
+      // Binds any click outside the headersearch div to removing the "active" class
+      // and therefore hidings the header's search dropdown.
+      headersearch.bind('clickoutside', function(event){
+        $(this).toggleClass('active');
 
-      // Unbinds the function so it can only happen once.
-      $(this).unbind('clickoutside');
+        // Unbinds the function so it can only happen once.
+        $(this).unbind('clickoutside');
+      });
     });
-  });
-}
+  }
 
-function headerSearchMobile() {
-  var headersearch = $("#mobile-header-search");
+  // Only run if the browser window size implies a mobile device.
+  if (windowwidth <= 700) {
+    $('#mobile-search-icon').on('click', function() {
+      $('body').bind('touchmove', function(e){
+        e.preventDefault();
+      });
 
-  // Runs the function when the header-search input is the focused element.
-  $("#mobile-search-icon").click(function() {
-    $(headersearch).addClass('active');
-    $('body').bind('touchmove', function(e){
-      e.preventDefault()
+      // Checks to make sure the "header-search" div doesn't already have an "active" class.
+      if (!headersearch.hasClass('active')) {
+        // Variable for moving up one in the HTML hierarchy.
+        var parentform = headersearchinput.parent('form');
+
+        // Moves up another level to the header-search div.
+        parentform.parent('#header-search').toggleClass('active');
+      }
     });
-
-    closeHeaderSearchMobile();
-  });
+  }
 }
 
 function closeHeaderSearchMobile() {
-  var headersearch = $("#mobile-header-search");
+  var headersearch = $("#header-search");
 
   $("#close-mobile-search-icon").click(function() {
     $(headersearch).removeClass('active');
@@ -48,16 +56,14 @@ function closeHeaderSearchMobile() {
   });
 }
 
-var ready = function() {  
+var ready = function() {
+  headerSearch();
+
   var windowwidth = $(window).width();
 
-  // Only run if the browser window size doesn't imply a mobile device.
-  if (windowwidth >= '700') {
-    headerSearch();
-  }
   // Only run if the browser window size implies a mobile device.
-  else {
-    headerSearchMobile();
+  if (windowwidth <= 700) {
+    closeHeaderSearchMobile();
   }
 };
 
