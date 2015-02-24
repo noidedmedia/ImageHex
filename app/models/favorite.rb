82327e8525962ebb159bc8b29a168406fe4dc40f
connorshea <connor.james.shea@gmail.com>
2015-02-23 20:43:03 -0700

    What's the worst that could happen?

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,7 +1,7 @@
 ## 
 # A Favorite is a type of collection that holds a users favorite images.
 class Favorite < Collection
-  before_validation :fill_name
+  before_validation :fill_name, :fill_desc
 
   ##
   # since this will only ever have 1 curator, we make a helpful
@@ -9,8 +9,14 @@ class Favorite < Collection
   def curator
     curators.first
   end
+
   protected
   def fill_name
     self.name = "#{self.curator.name.possessive} Favorites"
+  end
+
+  protected
+  def fill_desc
+    self.description = "Images favorited by #{self.curator.name}."
   end
 end
