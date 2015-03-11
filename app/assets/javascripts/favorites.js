cf0c7445da4b97e7b29a6f096b29e1bb0d453845
connorshea <connor.james.shea@gmail.com>
2015-03-10 22:52:37 -0600

    IT WORKS \o/

--- app/assets/javascripts/favorites.js
+++ app/assets/javascripts/favorites.js
@@ -1,14 +1,18 @@
 // When an AJAX request is sent successfully, the "favorited" class
 // is toggled on the element.
 function favoriteImage() {
-  $("#img-action-favorite").parent("a[data-remote]").on("ajax:success", function(e, data, status, xhr) {
-    if ($("#img-action-favorite").hasClass("favorited")) {
-      $("#img-action-favorite").toggleClass("favorited");
-      $("#img-action-favorite").html("Favorite");
-    } else {
-      $("#img-action-favorite").toggleClass("favorited");
-      $("#img-action-favorite").html("Favorited");
-    }
+  $("#img-action-favorite").children("a[data-remote]").on("ajax:success", function(e, data, status, xhr) {
+    $("#img-action-favorite").toggleClass("active");
+    $("#img-action-favorite").toggleClass("inactive");
+    $("#img-action-favorited").toggleClass("active");
+    $("#img-action-favorited").toggleClass("inactive");
+  });
+
+  $("#img-action-favorited").children("a[data-remote]").on("ajax:success", function(e, data, status, xhr) {
+    $("#img-action-favorite").toggleClass("active");
+    $("#img-action-favorite").toggleClass("inactive");
+    $("#img-action-favorited").toggleClass("active");
+    $("#img-action-favorited").toggleClass("inactive");
   });
 }
 
