c812f5445908d710c0bd5a19088e22e81bc1285c
connorshea <connor.james.shea@gmail.com>
2015-03-14 14:27:35 -0600

    Better explanation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   ##
   # This allows us to get a "link" to a comment, which is really just a link
-  # to the thing being commented on with the id of the comment appended with
-  # a #
+  # to the thing being commented on with the id of the comment appended to the
+  # URL as an anchor link.
   def comment_path(comment)
     polymorphic_path(comment.commentable) + "##{comment.id}"
   end
