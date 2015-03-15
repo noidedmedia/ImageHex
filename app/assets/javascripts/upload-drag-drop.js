d44ad9e705b7e45d0d799b878a41985f2a114ef5
connorshea <connor.james.shea@gmail.com>
2015-03-15 13:36:31 -0600

    Files handler for upload page

--- app/assets/javascripts/upload-drag-drop.js
+++
@@ -1,8 +0,0 @@
-var ready = function() {
-  if (window.location.href.search("images/new") >= 0) {
-    console.log("Upload page only!");
-    
-  }
-};
-
-$(document).ready(ready);
--- app/assets/javascripts/upload.js
+++ app/assets/javascripts/upload.js
@@ -1,6 +1,53 @@
+function fileUpload() {
+  $("#file-field").on("dragenter", function() {
+    console.log("Drag entered.");
+  });
+
+  $("#file-field").on("dragleave", function() {
+    console.log("Drag left.");
+  });
+
+  $("#file-field").on("drop", addedFiles);
+}
+
+function addedFiles(event) {
+    console.log("Drop.");
+
+    var files = event.originalEvent.dataTransfer.files;
+
+    console.log(files);
+
+    var file = [];
+
+    for (var i = 0; i < files.length; i++) {
+
+      file[i] = event.originalEvent.dataTransfer.files;
+
+      console.log(i);
+
+      console.log( file[i] );
+    }
+
+    return file;
+
+    handleFiles(file);
+  }
+
+function handleFiles(files) {
+  if (files) {
+    console.log("Display upload container.");
+    $(".upload-container").addClass("active");
+
+    console.log("Display submit button.");
+    $("#upload-submit-button").addClass("active");
+  }
+}
 
 var ready = function() {
-  
+  if (window.location.href.search("images/new") >= 0) {
+    console.log("Upload page only!");
+    fileUpload(); 
+  }
 };
 
 $(document).ready(ready);
