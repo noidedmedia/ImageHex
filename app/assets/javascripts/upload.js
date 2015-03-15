93aef67f249f2c0949b253cc239bd82a8fb94898
connorshea <connor.james.shea@gmail.com>
2015-03-15 13:50:09 -0600

    IT ACTUALLY WORKS

--- app/assets/javascripts/upload.js
+++ app/assets/javascripts/upload.js
@@ -15,23 +15,38 @@ function addedFiles(event) {
 
     var files = event.originalEvent.dataTransfer.files;
 
-    handleFiles(files);
+    console.log(files);
+
+    if (files) {
+      handleFiles(files);
+    }
   }
 
 function handleFiles(files) {
-  if (files) {
-    console.log("Display upload container.");
-    $(".upload-container").addClass("active");
+  console.log("Display upload container.");
+  $(".upload-container").addClass("active");
 
-    console.log("Display submit button.");
-    $("#upload-submit-button").addClass("active");
+  console.log("Display submit button.");
+  $("#upload-submit-button").addClass("active");
+
+  for (var i = 0; i < files.length; i++) {
+    $(".image-file-name").html(files[i].name);
+
+    var img = document.createElement("img");
+    img.src = window.URL.createObjectURL(files[i]);
+    img.width = 348;
+    img.onload = function() {
+      window.URL.revokeObjectURL(this.src);
+    }
+
+    $(".image-thumbnail").html(img);
   }
 }
 
 var ready = function() {
   if (window.location.href.search("images/new") >= 0) {
     console.log("Upload page only!");
-    fileUpload(); 
+    fileUpload();
   }
 };
 
