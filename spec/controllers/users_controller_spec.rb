64cc3f47d1a3c619a8333f0cdba2c4d72dc86cad
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-03-21 12:07:30 -0600

    Test Changes
    * Changed test to use /users/edit
    * Changed test image to something smaller (and not a pony reference,
    * which I am kinda regretting--how else am I supposed to piss off
    * Connor?)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
-
+  include Devise::TestHelpers
+  context "when logged in" do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      @user.confirm!
+      sign_in @user
+    end
+    describe "get #edit" do
+      it "sets the user variable" do
+        get :edit, id: @user.id
+        expect(assigns(:user)).to eq(@user)
+      end
+      it "responds with success" do
+        get :edit, id: @user.id
+        expect(response).to be_success
+      end
+    end
+    describe "put #update" do
+      describe "updating user page" do
+        it "allows updating" do
+          put :update, id: @user.id, user: {page: "test"}
+          expect(@user.user_page.body).to eq("test")
+        end
+      end
+      describe "updating avatar" do
+        it "allows updating" do
+          img = FactoryGirl.create(:image)
+          put :update, id: @user.id, user: {avatar_id: @image.id}
+          expect(@user.avatar).to eq(img)
+        end
+      end
+    end
+  end
 end
--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :image do
     user
-    f { Rack::Test::UploadedFile.new(Rails.root.join("spec", "fixtures", "files", "test.png"), "image/png")}
+    f { Rack::Test::UploadedFile.new(Rails.root.join("spec", "fixtures", "files", "test.jpg"), "image/jpg")}
     license "Public Domain"
     medium :photograph
   end
