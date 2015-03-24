31ce2881ffa63f1d266b2d37f845b516448c44dd
connorshea <connor.james.shea@gmail.com>
2015-03-24 17:18:21 -0600

    :D It works!

--- app/assets/javascripts/metadata.js
+++ app/assets/javascripts/metadata.js
@@ -1,17 +1,29 @@
-function metadataTest() {
-  console.log(EXIF);
-
+function metadataParse() {
   EXIF.getData(this, function() {
     console.log(EXIF.pretty(this));
 
-    // var make = EXIF.getTag(this, "Make"),
-    //     model = EXIF.getTag(this, "Model");
-    // console.log("I was taken by a " + make + " " + model);
+    var image = this;
+
+    populateInfo(image);
   });
 };
 
+function populateInfo(image) {
+  if ( EXIF.getTag(image, "ImageWidth") != undefined ) {
+    var width = EXIF.getTag(image, "ImageWidth"),
+        height = EXIF.getTag(image, "ImageHeight");
+  } else if ( EXIF.getTag(image, "PixelXDimension") != undefined ) {
+    var width = EXIF.getTag(image, "PixelXDimension"),
+        height = EXIF.getTag(image, "PixelYDimension");
+  } else {
+    console.log("So it has come to this.");
+  }
+
+  console.log("Dimensions: " + width + "x" + height);
+}
+
 var ready = function() {
-  $(".image > img").on("load", metadataTest);
+  $(".image > img").on("load", metadataParse);
 };
 
 $(document).ready(ready);
