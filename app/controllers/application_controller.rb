64a6b23a5d87142de7a7002331b377fc3412eb36
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-03-26 20:16:04 -0600

    Honestly I barely know

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,6 +32,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # Set the locale in all our URLS
+  def default_url_options(options = {})
+    { locale: I18n.locale}.merge options
+  end
   def configure_devise_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
   end
