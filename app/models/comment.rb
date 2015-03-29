742104244d14e6a1fa819ce90e63923f7b95f699
connorshea <connor.james.shea@gmail.com>
2015-03-29 13:04:24 -0600

    i18n notifications

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -53,7 +53,7 @@ class Comment < ActiveRecord::Base
   end
 
   def image_reply_message
-    "#{user.name} made a comment on ##{commentable.id}"
+    I18n.t 'made_a_comment', username: "#{user.name}", commentable: "##{commentable.id}"
   end
   ##
   # Notify reply tells us to make a notification of a reply to
@@ -71,8 +71,9 @@ class Comment < ActiveRecord::Base
   # Notification message just returns a string to use as the message
   # in a notification
   def reply_message(other)
-    "#{other.user.name} replied to your comment on #{commentable_type} ##{commentable_id}"
+    I18n.t 'replied_to_your_comment', username: "#{other.user.name}", commentable_type: "#{commentable_type}", commentable: "##{commentable.id}"
   end
+
   ##
   # You can reply to another comment 4chan-style, by typing
   #     >>#{id of other comment}
@@ -105,7 +106,7 @@ class Comment < ActiveRecord::Base
   end
 
   def mention_message(user)
-    "#{self.user.name} mentioned you in a comment"
+    I18n.t 'mentioned_you', username: "#{self.user.name}"
   end
 
 end
