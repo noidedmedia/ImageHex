897e05f2074896fae9c406c819018fccb248ef0b
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-03-29 18:28:55 -0600

    pls

--- app/models/image.rb
+++ app/models/image.rb
@@ -121,7 +121,7 @@ class Image < ActiveRecord::Base
 
   def downcase_extension
     ext = File.extname(f_file_name).downcase
-    base = File.basename(f_file_name, File.extname(f_file_name)).downcase
+    base = File.basename(f_file_name, ".#{File.extname(f_file_name)}").downcase
     self.f.instance_write :file_name, "#{base}.#{ext}"
   end
 end
