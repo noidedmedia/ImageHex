c745d7a8b49680685f1b6b55b6df3971e530337f
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-05-09 11:34:25 -0600

    Actually fix username factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-   name { Faker::Internet.user_name, "_"}
+   name { Faker::Internet.user_name(nil, %w(_))}
     email { Faker::Internet.email }
     password { Faker::Internet.password(8) }
 
