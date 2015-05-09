687ff597f974c57229926c660105ba4b6ab141e2
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-05-09 11:32:57 -0600

    Fixed user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-   name { Faker::Internet.user_name}
+   name { Faker::Internet.user_name, "_"}
     email { Faker::Internet.email }
     password { Faker::Internet.password(8) }
 
