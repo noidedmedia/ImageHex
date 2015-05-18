2455818d36aff72c169862f07060b0cd99eb9cae
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-05-18 13:20:25 -0600

    Initial migration
    Add description to tags in order to be cool

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503185836) do
+ActiveRecord::Schema.define(version: 20150518191924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(version: 20150503185836) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "display_name"
+    t.text     "description"
   end
 
   create_table "user_pages", force: :cascade do |t|
