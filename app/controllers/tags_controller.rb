5c0fe4eca5298093f7689677aa203ef646e7c4d7
Anthony Super <subtlehyperbole.thesuper@gmail.com>
2015-05-18 13:56:12 -0600

    Backend stuff to update tags
    Gotta make it all markdown-ey, but otherwise it works

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,7 @@
 # A single-action controller used for tag suggestion.
 # TODO: Allow users to modify information about tags in this controller.
 class TagsController < ApplicationController
+  before_filter :ensure_user, only: [:edit, :update]
   ##
   # Given a partial tag name in "params['name']", suggests ten possible
   # completed tags in alphabetical order.
@@ -19,4 +20,22 @@ class TagsController < ApplicationController
     @tags = Tag.all.paginate(page: page, per_page: per_page)
   end
 
+  def edit
+    @tag = Tag.friendly.find(params[:id])
+  end
+
+  def update
+    tag = Tag.friendly.find(params[:id])
+    if tag.update(tag_params)
+      redirect_to tag
+    else
+      flash[:warning] = tags.errors.full_messages
+      redirect_to action: :edit
+    end
+
+  end
+  protected
+  def tag_params
+    params.require(:tag).permit(:description)
+  end
 end
