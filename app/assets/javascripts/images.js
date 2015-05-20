d40c0895b152c333f1f40be30c20ea3de545651a
connorshea <connor.james.shea@gmail.com>
2015-05-19 19:45:18 -0600

    I guess I do not in fact need the js-routes gem. Yay.

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -12,8 +12,36 @@ function cancelReportButton() {
   });
 }
 
+function addToCollection() {
+  // Intercepts submission of the "Add to Collection" button press and does it remotely.
+  $(".add-to-collection-form").submit(function(e) {   
+    e.preventDefault();
+
+    var data = $(this).serialize();
+    
+    // For use in the AJAX request below.
+    var _this = this;
+
+    // http://api.jquery.com/jquery.ajax/
+    $.ajax({
+      type: "POST",
+      url: this.action,
+      data: data,
+      success: function() {
+        // Hides the Collection Tooltip once the request has completed.
+        $("#img-action-collection-tooltip").removeClass("active").addClass("inactive");
+
+        // Use this once the Collection List function checks to see if the image
+        // is already a member of each collection.
+        // $(_this).addClass("check");
+      }
+    });
+  });
+}
+
 // Functions run when the document is "ready".
 $(document).ready(function() {
   deleteButtonAlert();
   cancelReportButton();
+  addToCollection();
 });
