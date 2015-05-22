636a17ab361ced8f93d4c6b7450f67e5e14ca483
connorshea <connor.james.shea@gmail.com>
2015-05-21 21:46:06 -0600

    Updating notifications.js to run asynchronously.
    Line #30 in `notifications.js` is the problem.

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,17 +1,16 @@
-function notificationsDropdown() {
-  $(".notifications-unread-count a").on("click", function(event) {
-    event.preventDefault();
-    $(".notifications-dropdown").toggleClass("active").toggleClass("inactive");
-    $(".header-notifications").toggleClass("active");
+function notificationsDropdownDisplay() {
+  $(".notifications-dropdown").toggleClass("active").toggleClass("inactive");
+  $(".header-notifications").toggleClass("active");
 
-    $(".header-notifications").bind("clickoutside", function() {
-      $(".notifications-dropdown").removeClass("active").addClass("inactive");
-      $(".header-notifications").removeClass("active");
+  $(".header-notifications").bind("clickoutside", function() {
+    $(".notifications-dropdown").removeClass("active").addClass("inactive");
+    $(".header-notifications").removeClass("active");
 
-      $('.header-notifications').unbind("clickoutside");
-    });
+    $('.header-notifications').unbind("clickoutside");
   });
+}
 
+function markAllRead() {
   $(".mark-all-read").on("click", function() {
     $(".notifications-unread-count a").html("0");
     $(".header-notifications").removeClass("unread");
@@ -22,13 +21,30 @@ function notificationsDropdown() {
   });
 }
 
+function notificationsDropdownGet() {
+  $(".notifications-unread-count a").on("click", function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      type: "GET",
+      url: "/notifications/unread",
+      dataType: "json",
+      success: function(result) {
+        console.log(result);
+        notificationsDropdownDisplay();
+      }
+    });
+  });
+}
+
 var ready = function() {
 
   var windowwidth = $(window).width();
 
   // Only run if the browser window doesn't imply a mobile device.
   if (windowwidth > '750') {
-    notificationsDropdown();
+    notificationsDropdownGet();
+    markAllRead();
   }
 };
 
