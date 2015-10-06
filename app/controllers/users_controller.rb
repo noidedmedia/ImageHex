0d47e38767482898a031ee6e536d511c2d43196f
connorshea <connor.james.shea@gmail.com>
2015-10-05 20:21:00 -0600

    Image `show_stub` view is now able to accept a variable
    No more locking into `@images`!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,11 @@ class UsersController < ApplicationController
   # 
   # Sets the following variables:
   # @user:: The user we're viewing.
-  # @images:: Images the user has uploaded.
+  # @uploads:: Images the user has uploaded.
   # @collections:: Collections the user curates.
   def show
     @user = User.friendly.find(params[:id])
-    @images = @user.images
+    @uploads = @user.images
       .paginate(page: page, per_page: per_page)
       .for_content(content_pref)
     @collections = @user.collections
