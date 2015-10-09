d9f81923c641ea5d39a7b70eed0fc5901b3812a5
connorshea <connor.james.shea@gmail.com>
2015-10-08 18:27:39 -0600

    Profile picture preview in Settings works now!
    Press and Help links also no longer 404, but I wouldn't call them
    “working” by any means

---
+++ app/assets/javascripts/settings.js
@@ -0,0 +1,27 @@
+function avatarUploader() {
+  $("#avatar-uploader").on("change", avatarUploaderPreview);
+}
+
+
+function avatarUploaderPreview(event) {
+  var files = event.target.files || (event.originalEvent.dataTransfer && event.originalEvent.dataTransfer.files);
+
+  console.log(files);
+
+  if (files) {
+    var img = document.createElement("img");
+    img.src = window.URL.createObjectURL(files[0]);
+    img.width = 150;
+    img.onload = function() {
+      window.URL.revokeObjectURL(this.src);
+    };
+
+    $("#avatar-uploader-thumbnail-container").html(img);
+  }
+}
+
+var ready = function() {
+  avatarUploader();
+};
+
+$(document).ready(ready);
