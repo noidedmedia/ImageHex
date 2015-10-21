6f458351b2d208a66818e10374bc9309bda1f2e5
connorshea <connor.james.shea@gmail.com>
2015-10-21 12:48:06 -0600

    Removing sanitizer for opt_attempt
    Not sure how to fix this issue other than removing this

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -59,12 +59,11 @@ class ApplicationController < ActionController::Base
     return options if I18n.locale == I18n.default_locale
     { locale: I18n.locale }.merge options
   end
-  
+
   ##
   # Allow devise to add a user's name on creation.
   def configure_devise_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_paramater_sanitizer.for(:sign_up) << :otp_attempt
   end
 
   DEFAULT_CONTENT = {
