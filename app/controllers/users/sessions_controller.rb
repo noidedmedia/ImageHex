4acd52f98347bdd70f41b7a3f3ec8d7dcfcc9453
Anthony <anthony@noided.media>
2015-10-23 10:28:38 -0600

    Fix a Typo

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -25,7 +25,7 @@ class Users::SessionsController < Devise::SessionsController
       set_flash_message(:notice, :signed_in) if is_flashing_format?
       sign_in(resource_name, resource)
       yield resource if block_given?
-      espond_with resource, location: after_sign_in_path_for(resource)
+      respond_with resource, location: after_sign_in_path_for(resource)
     end
   end
 
