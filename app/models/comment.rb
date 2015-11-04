702723bf86e00dc710e4c5be68674c43a622528b
Anthony <anthony@noided.media>
2015-11-04 11:44:40 -0700

    Cannot mention yourself in a comment anymore
    That's kind of nice.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -93,7 +93,7 @@ class Comment < ActiveRecord::Base
     names = body.scan(/@\w+/)
       .map{|m| m.gsub("@", "")}
     users = User.where(name: names)
-    users.each{|u| notify_mention(u)}
+    users.each{|u| notify_mention(u) unless u == self.user}
   end
 
   ##
