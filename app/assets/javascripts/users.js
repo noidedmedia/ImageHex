5616de7f3de7754c3a7ced82536be8818d54779a
connorshea <connor.james.shea@gmail.com>
2015-11-08 14:47:30 -0700

    Figured out how to get `users.js` to work
    I guess I don't need a `for` loop after all :D

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,10 +4,16 @@ function tabbedMenu() {
     tabs[i].addEventListener('click', function(e) {
       var tabid = e.target.getAttribute('data-tab');
 
-      document.querySelectorAll('ul#user-page-header-tabs li').classList.remove('current');
-      document.querySelectorAll('.tab-content').classList.remove('current');
+      // De-emphasize currently selected tab.
+      document.querySelector('#user-page-header-tabs li.current').classList.remove('current');
 
-      document.querySelector(e.target).classList.add('current');
+      // Hide current tab contents.
+      document.querySelector('.tab-content.current').classList.remove('current');
+
+      // Set tab element that was clicked to the current tab.
+      e.target.classList.add('current');
+
+      // Display it's respective tab contents.
       document.querySelector('.tc-' + tabid).classList.add('current');
     });
   };
