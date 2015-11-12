2ffb1d62116d04ffa032d164db7ad2c30f5584b6
connorshea <connor.james.shea@gmail.com>
2015-11-11 19:23:09 -0700

    Well I guess this was all for naught.

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -4,6 +4,7 @@ class Admin::ImagesController < Admin::AdminController
   def live
 
   end
+
   ##
   # Show all images that have been reported.
   # Sets the following variables:
@@ -14,14 +15,17 @@ class Admin::ImagesController < Admin::AdminController
 
   ##
   # Delete an image for violating some of the almighty rules on ImageHex.
-  # Redirects back to /admin/images so you can delete some more.
+  # Redirects back to `/admin/images` so you can delete some more.
+  # @image:: The image being deleted.
   def destroy
     @image = Image.find(params[:id]).destroy
     redirect_to "/admin/images"
   end
+  
   ##
-  # Remove all reports on the image in params[:id]
+  # Remove all reports on the image in params[:id].
   # Used if people are reporting stuff they shouldn't.
+  # @image:: The image being absolved.
   def absolve
     @image = Image.find(params[:id])
     @image.image_reports.update_all(active: false)
