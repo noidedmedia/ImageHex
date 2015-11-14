836eb20be9608c83441009630ee7886e1c407693
Anthony <anthony@noided.media>
2015-11-14 13:19:36 -0700

    Fix a screwup

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-
+require 'pry'
 RSpec.describe Comment, :type => :model do
   it "should require user to be set" do
     expect(FactoryGirl.build(:comment,
@@ -22,16 +22,17 @@ RSpec.describe Comment, :type => :model do
   end
   it "makes a notification when replying" do
     c = FactoryGirl.create(:comment)
+    binding.pry
     expect{
       FactoryGirl.create(:comment, body: ">>#{c.id}:")
-    }.to change{Notification.count}.by(1)
+    }.to change{Notification.count}.by(2) # also generates an image reply
   end
   it "makes a notification when mentioning a user" do
     u = FactoryGirl.create(:user)
     comment_str = "@#{u.name}"
     expect{
       FactoryGirl.create(:comment, body: comment_str)
-    }.to change{Notification.count}.by(1)
+    }.to change{Notification.count}.by(2) # also generates an image reply
   end
 
 end
