acab9c81bd755ef9d8b2c7beca904221ebe6a0a3
Anthony Super <anthony@noided.media>
2015-12-06 19:30:58 -0700

    Fix tag ordering

--- app/models/tag_group.rb
+++ app/models/tag_group.rb
@@ -19,7 +19,7 @@ class TagGroup < ActiveRecord::Base
   # RELATIONSHIPS #
   #################
   belongs_to :image
-  has_many :tags, through: :tag_group_members
+  has_many :tags, ->{by_importance},  through: :tag_group_members
   has_many :tag_group_members
   has_many :tag_group_changes
   ###############
