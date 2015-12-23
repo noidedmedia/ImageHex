5caa2c79e1994c74370c565e34d6425f320fdd9b
Anthony Super <anthony@noided.media>
2015-12-23 11:09:33 -0700

    More config for stripe

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -10,6 +10,12 @@ class StripeController < ApplicationController
   end
   def callback
     code = params[:code]
-    @resp = OAUTH_CLIENT.auth_code.get_token(code, params: {scope: "read_write"})
+    @resp = OAUTH_CLIENT.auth_code.get_token(code, 
+                                             params: {scope: "read_write"})
+    params = {
+      :stripe_publishable_key => @resp["stripe_publishable_key"]
+      :stripe_user_id => @resp["stripe_user_id"]
+      :stripe_access_token => @resp.token
+    }
   end
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -6,4 +6,9 @@ options = {
 OAUTH_CLIENT = OAuth2::Client.new(ENV['STRIPE_CLIENT_ID'],
                                   ENV['STRIPE_SECRET_KEY'],
                                   options)
+Rails.configuration.stripe = {
+  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+  secret_key: ENV['STRIPE_SECRET_KEY']
+}
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
