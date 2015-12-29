f9cc64a66b330ac74af4ec670655200fef4cf1d1
Anthony Super <anthony@noided.media>
2015-12-29 10:28:40 -0700

    Use React to handle the inputs

--- app/assets/javascripts/components/commissions_plan.es6.jsx
+++ app/assets/javascripts/components/commissions_plan.es6.jsx
@@ -1,12 +1,19 @@
 class CommissionCalculatorResults extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      value: ""
+    }
   }
-  
-  render() {
-    var total = document.getElementById("commission-calculator-input").value;
 
-    total = total * 100;
+  render() {
+    var input = <input type="number"
+      value={this.state.value}
+      min="3"
+      id="commission-calculator-input"
+      onChange={this.updateValue.bind(this)}
+    />;
+    total = this.state.value * 100;
 
     var stripeFees = Math.floor((total * 0.029) + 30);
     var imagehexFees = Math.floor((total * 0.091));
@@ -14,26 +21,30 @@ class CommissionCalculatorResults extends React.Component {
 
     if (total >= 300) {
       return <div>
+        {input}
         <p>Stripe: ${(stripeFees / 100).toFixed(2)}</p>
         <p>ImageHex: ${(imagehexFees / 100).toFixed(2)}</p>
         <p>Artist Earnings: ${(artistEarnings / 100).toFixed(2)}</p>
       </div>;
     }
-    
+
     return <div>
+      {input}
       <p></p>
     </div>;
+  }
+  updateValue(event){
+    this.setState({
+      value: event.target.value
+    });
   }
 }
 
 var ready = function() {
-  if (document.getElementById("commission-calculator-input-container")) {
-    document.getElementById("commission-calculator-input").addEventListener('input', function() {
-      ReactDOM.render(
-        <CommissionCalculatorResults/>,
-        document.getElementById("commission-calculator-results-container")
-      );
-    });
+  var x;
+  if (x = document.getElementById("commission-calculator-input-container")) {
+    ReactDOM.render(<CommissionCalculatorResults/>,
+                    x);
   }
 };
 
