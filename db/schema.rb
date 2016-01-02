e9b5b38165e58e915c0c7865f3b2284433f24663
Anthony Super <anthony@noided.media>
2016-01-02 13:06:09 -0700

    Add body to messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160101202827) do
+ActiveRecord::Schema.define(version: 20160102200322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -238,6 +238,7 @@ ActiveRecord::Schema.define(version: 20160101202827) do
     t.datetime "read_at"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.text     "body"
   end
 
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
