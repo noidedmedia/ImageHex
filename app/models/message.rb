243be34f52f7c49b920190745dfeea2b20435153
Anthony Super <anthony@noided.media>
2016-01-03 13:54:11 -0700

    Fix scopes [skip ci]

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,19 @@ class Message < ActiveRecord::Base
   belongs_to :user
   belongs_to :conversation
 
+
+  ##
+  # The next two methods are kind of hacks
+  # Basically, we somtimes select a calcualted READ value in SQL, and
+  # sometimes we don't. So we basically fake having it *always* selected
+  # by defining dummy methods
+  def read?
+    attributes["read"]
+  end
+
+  def read
+    attributes["read"]
+  end
   ##########
   # SCOPES #
   ##########
@@ -9,7 +22,7 @@ class Message < ActiveRecord::Base
     joins("INNER JOIN conversation_users ON conversation_users.conversation_id = messages.conversation_id")
       .where(conversation_users: {user_id: user.id})
       .where("messages.created_at > conversation_users.last_read_at")
-      .order("messages.conversation_id")     
+      .select("messages.*, false AS read")
   end
 
   def self.with_read_status_for(user)
--- app/views/messages/_message.json.jbuilder
+++ app/views/messages/_message.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! message, :id, :body, :conversation_id, :created_at
+json.extract! message, :id, :body, :conversation_id, :created_at, :read?
 json.user message.user, partial: "users/stub", as: :user
