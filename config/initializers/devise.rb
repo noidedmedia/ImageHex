fc37b258694798c5716de6b0d805ae8e5d795d98
Anthony Super <anthony@noided.media>
2016-01-06 14:31:06 -0700

    Change devise mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,7 +19,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'admin@imagehex.com'
-  config.mailer = "UserMailer"
+  config.mailer = "DeviseMailer"
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
