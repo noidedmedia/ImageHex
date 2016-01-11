65c41b55b0dbc6875ff525cb35228eb6894be4c6
Anthony Super <anthony@noided.media>
2016-01-11 10:32:05 -0700

    Change how the subjects section of commission products work

--- app/assets/javascripts/components/product_subject_input.es6.jsx
+++ app/assets/javascripts/components/product_subject_input.es6.jsx
@@ -11,86 +11,50 @@ class ProductSubjectInput extends React.Component {
     var includedInput = <span>
       <input type="number"
         step="1"
-        onChange={this.changeNumberIncluded.bind(this)}
-        value={this.state.includedSubjects} 
-        min={this.state.minimumIncluded} />
+        name="commision_product[included_subjects]"
+        min={this.findMinimumIncluded()} />
     </span>;
-    if(this.state.offerSubjects){
-      return <div>
-        <h3>Charge for Subjects</h3>
-        This product will come with {includedInput} subjects free-of-charge.
-        Each additional subject, up to a maximum of
-        <input type="number"
-          step="1.0"
-          onChange={this.changeMaximum.bind(this)}
-          value={this.state.maximumSubjects} />,
-        will cost
-        <CurrencyInputField
-          min={1.00}
-          initialValue={this.props.subjectPrice}
-          name="commission_product[subject_price]" />.
-        You can also choose to <a onClick={this.disallowSubjects.bind(this)}>
-          limit the commission to a certain number of subjects,
-          included in the base price.
-        </a>
-        <input type="hidden"
-          name="commission_product[maximum_subjects]"
-          value={this.state.maximumSubjects} />
-        <input type="hidden"
+    var chargeSection = <ProductSubjectInput.ChargedSubjectSection />;
+    var disallowChargeSection = <input type="hidden"
+      name="commission_product[offer_subjects]"
+      value="false" />;
+    return <div>
+      <h3>Included Subjects</h3>
+      This commission will include {includedInput} subjects, free of charge.
+      <div className="checkbox-container">
+        <label>Charge for additional subjects</label>
+        <input type="checkbox"
           name="commission_product[offer_subjects]"
-          value="true" />
-      </div>;
-    }
-    else{
-      return <div>
-        <h3>Included Subjects</h3>
-        This commission will include {includedInput} subjects in the base price.
-        The client will not be allowed to add additional subjects, and will
-        not recieve a discount for adding less than this amount.
-        You can also chose to <a onClick={this.allowSubjects.bind(this)}>
-          charge clients on a per-subject basis.
-        </a>
-        <input type="hidden"
-          name="commission_product[offer_subjects]"
-          value="false" />
+          value={this.state.allowAdditional}
+          onChange={this.toggleAdditional.bind(this)} />
       </div>
-    }
+      {this.state.allowAdditional ? chargeSection : disallowChargeSection}
+    </div>;
   }
-  disallowSubjects(){
-    var n = this.state.includedSubjects;
-    if(n < 1){
-      n = 1;
-    }
+  toggleAdditional(event){
+    console.log(`Targeting additional to ${event.target.checked}`);
     this.setState({
-      offerSubjects: false,
-      minimumIncluded: 1,
-      includedSubjects: n
+      allowAdditional: event.target.checked
     });
   }
-  allowSubjects(){
-    this.setState({
-      offerSubjects: true,
-      minimumIncluded: 0,
-    });
+  findMinimumIncluded(){
+    return this.state.allowAdditional ? 0 : 1;
   }
-  changeNumberIncluded(event){
-    var n = parseInt(event.target.value);
-    if(n < 0){
-      n = 0;
-    }
-    console.log("Changing the number included to",n);
-    this.setState({
-      includedSubjects: n
-    });
-  }
-  changeMaximum(event){
-    var n = parseInt(event.target.value);
-    if(n < 1){
-      n = 1;
-    }
-    console.log("Changing the maximum number of subjects to", n);
-    this.setState({
-      maximumSubjects: n
-    });
-  }
+
 }
+
+ProductSubjectInput.ChargedSubjectSection = () => {
+  return <div>
+    Charge 
+    <CurrencyInputField
+      min={1.00}
+      initialValue="5.0"
+      name="commission_product[subject_price]" />
+    for each additional subject, to a max of
+    <input type="number"
+      step="1.0"
+      name="commission_product[maximum_subjects]"
+      initialValue="1"
+      min="1" />
+  </div>;
+};
