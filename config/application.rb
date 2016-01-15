d8bb911e4781710fa8f275558dec85f013780191
Anthony Super <anthony@noided.media>
2016-01-15 13:20:07 -0700

    Try to fix CircleCI tests

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module ImageHex
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = "UTC"
 
     # Custom i18n routes.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
