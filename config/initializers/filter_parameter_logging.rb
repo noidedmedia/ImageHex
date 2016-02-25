899e4f94cd8370de353ba029cde40631cd31f727
connorshea <connor.james.shea@gmail.com>
2016-02-25 11:51:13 -0700

    Run `rake rails:update`

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :encrypted_password, :encrypted_otp_secret, :encrypted_otp_secret_iv, :encrypted_otp_secret_salt, :otp_backup_codes, :otp_attempt, :otp_backup_attempt, :confirmation_token]
+Rails.application.config.filter_parameters += [:password, :otp_attempt]
