26f0f143ca0061c13c5c4c776d013dcafa20a77f
Anthony Super <anthony@noided.media>
2016-03-22 10:24:12 -0600

    Fix test

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 
 RSpec.describe Message, type: :model do
   describe "scopes" do
-    describe ".posted_since" do
+    describe ".created_since" do
       it "gives messages posted since a certain date" do
         b = create(:message)
         a = create(:message)
-        expect(Message.posted_since(b.created_at)).to eq([a])
+        expect(Message.created_since(b.created_at)).to eq([a])
       end
     end
   end  
