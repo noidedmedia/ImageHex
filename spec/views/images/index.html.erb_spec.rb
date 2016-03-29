a68b829fcdff1b36194eb4bbbf872aea3597a252
Anthony Super <anthony@noided.media>
2016-03-29 10:40:53 -0600

    Basic view test
    More of a test of those tests

--- spec/views/images/index.html.erb_spec.rb
+++ spec/views/images/index.html.erb_spec.rb
@@ -2,4 +2,13 @@
 require 'spec_helper'
 
 describe "images/index.html.erb" do
+  let(:images) do
+    10.times.map { create(:image) } 
+  end
+  it "displays all the images" do
+    assign(:images, images.paginate(per_page: 10))
+    render
+    expect(rendered).to have_selector(".image-gallery-item",
+                                      count: 10)
+  end
 end
