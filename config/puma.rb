a259ea36326d0645932108722096aede41fd71b1
Anthony Super <anthony@noided.media>
2016-06-30 10:53:39 -0600

    Fix puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 if ENV.fetch("RAILS_ENV") { "development" } != "development"
-
+  threads_count = Integer(ENV["RAILS_MAX_THREADS"] || 5)
   workers Integer(ENV['WEB_CONCURRENCY'] || 2)
   threads threads_count, threads_count
 
