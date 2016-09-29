9788e8f3981862f5320aff06a13228487c16afed
Anthony Super <anthony@noided.media>
2016-09-29 09:49:30 -0600

    Start on image preseneter

--- app/controllers/commissions_dashboard_controller.rb
+++ app/controllers/commissions_dashboard_controller.rb
@@ -1,4 +1,6 @@
 class CommissionsDashboardController < ApplicationController
+  before_action :ensure_user
   def index
+    @presenter = CommissionsPresenter.new(current_user)
   end
 end
---
+++ app/presenters/commissions_presenter.rb
@@ -0,0 +1,58 @@
+class CommissionsPresenter
+  def initialize(current_user)
+    @user = current_user
+  end
+
+  def placed_orders
+    @placed_orders ||= OrdersPresenter.new(@user.orders)
+  end
+
+  def recieved_orders
+    @recieved_orders ||= OrdersPresenter.new(Order.to_user(@user).confirmed)
+  end
+
+  def listings
+    @listings ||= ListingsPresenter.new(@user.listings.includes(:images).to_a)
+  end
+
+  class OrdersPresenter
+    def initialize(orders)
+      @orders = orders
+    end
+
+    def completed
+      @orders.select{|o| o.image.present?}
+    end
+
+    def rejected
+      @rejected ||= @orders.select(&:rejected?)
+    end
+
+    def active
+      @active ||= @orders.select{|o| o.accepted? && o.image_id.nil?}
+    end
+
+    def pending
+      @pending ||= @orders.select{|o| ! o.accepted? && ! o.rejected?}
+    end
+
+    def method_missing(meth, *args)
+      @orders.public_send(meth, *args)
+    end
+  end
+
+  class ListingsPresenter
+    def initialize(listings)
+      @listings = listings
+    end
+
+    def open
+      @listings.select(&:open?)
+    end
+
+
+    def unopen
+      @listings.reject(&:open?)
+    end
+  end
+end
