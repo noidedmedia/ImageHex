425c434f3c0da6f26d2320def3858d134354684d
Anthony Super <anthony@noided.media>
2016-09-29 09:59:38 -0600

    remove test flakiness

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@
 FactoryGirl.define do
   factory :user do
     name { Faker::Internet.user_name(nil, %w(_)) }
-    email { "#{name}@example.com" }
+    sequence(:email) {|n| "#{name}#{n}@example.com" }
     password { Faker::Internet.password(8) }
     after(:build) do |user|
       user.skip_confirmation!
