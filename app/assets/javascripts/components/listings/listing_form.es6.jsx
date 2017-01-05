45b2b44d6c7a7ee7da3a6229d99085fb3853b777
Anthony Super <anthony@noided.media>
2017-01-05 16:01:15 -0700

    Rip out everything

--- app/assets/javascripts/components/listings/listing_form.es6.jsx
+++ app/assets/javascripts/components/listings/listing_form.es6.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import PriceSection from './price_section.es6.jsx';
-import OptionSection from './option_section.es6.jsx'; 
-import CategorySection from './category_section.es6.jsx';
 import ExampleImageSection from './example_image_section.es6.jsx';
 import ReactUJS from '../../react_ujs';
 
@@ -10,8 +7,6 @@ class ListingForm extends React.Component {
     super(props);
     this.state = {
       ...props,
-      removedCategories: [],
-      removedOptions: []
     };
     this.categoryFakeId = -1;
     this.fakeOptionId = -1;
@@ -19,90 +14,12 @@ class ListingForm extends React.Component {
 
   render() {
     return <div id="listing-form">
-      <PriceSection {...this.state}
-        toggleCheck={this.toggleCheck.bind(this)} />
-      <CategorySection
-        categories={this.state.categories}
-        addCategory={this.addCategory.bind(this)}
-        removedCategories={this.state.removedCategories}
-        removeCategory={this.removeCategory.bind(this)} />
-      <OptionSection
-        quoteOnly={this.state.quote_only}
-        addOption={this.addOption.bind(this)}
-        removeOption={this.removeOption.bind(this)}
-        removedOptions={this.state.removedOptions}
-        options={this.state.options} />
       <ExampleImageSection />
-        {this.submitSection}
-      </div>
-    }
-
-  get submitSection() {
-    if(this.state.categories.length == 0) {
-      return <div className="warning-dialogue">
-        <span className="warning-dialogue-icon" />
-        <span className="warning-dialogue-inner">
-          You must add one reference category, which ImageHex uses to organize reference material.
-          Typically, this will represent objects or characters in the image.
-        </span>
-      </div>;
-    }
-    else {
-      return  <button type="submit"
-        className="checkmark-submit-button">
-        Save Listing
-      </button>;
-    }
-  }
-
-
-  getChildContext() {
-    return {
-      quoteOnly: this.state.quote_only
-    };
-  }
-
-  toggleCheck() {
-    this.setState({
-      quote_only: ! this.state.quote_only
-    });
-  }
-
-  addOption() {
-    this.setState({
-      options: [...this.state.options, {id: this.fakeOptionId}]
-    });
-    this.fakeOptionId--;
-  }
-
-  removeOption(index) {
-    let option = this.state.options.splice(index, 1)[0];
-    console.log("Removed option",option);
-    let ro = option.id > 0 ? [...this.state.removedOptions, option] : (
-      this.state.removedOptions)
-    this.setState({
-      options: this.state.options,
-      removedOptions: ro
-    });
-  }
-
-  addCategory() {
-    this.setState({
-      categories: [...this.state.categories, {id: this.categoryFakeId}]
-    });
-    this.categoryFakeId = this.categoryFakeId - 1;
-  }
-
-  removeCategory(index) {
-    let removed = this.state.categories.splice(index, 1)[0];
-    console.log("Removed category",removed);
-    let removedCategories = removed.id > 0 ? (
-      [...this.state.removedCategories, removed]) : (
-        this.state.removedCategories);
-    this.setState({
-      categories: this.state.categories,
-      removedCategories
-    });
+    <button type="submit"
+      className="checkmark-submit-button">
+      Save Listing
+    </button>
+    </div>;
   }
 }
 
--- app/views/listings/show.json.jbuilder
+++ app/views/listings/show.json.jbuilder
@@ -1,26 +1,6 @@
 json.extract! @listing,
   :name,
   :description,
-  :base_price,
-  :quote_only,
   :created_at
-
-json.options @listing.options do |option|
-  json.extract! option,
-    :id,
-    :price,
-    :name,
-    :description
-end
-
-json.categories @listing.categories do |category|
-  json.extract! category,
-    :id,
-    :price,
-    :name,
-    :description,
-    :max_count,
-    :free_count
-end
 
 json.images @listing.images, partial: "images/stub", as: :image
