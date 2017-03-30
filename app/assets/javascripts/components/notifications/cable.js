b8f23884ceb35be27e6a8904334c99eccc3c60fd
Anthony Super <anthony@noided.media>
2017-03-29 20:47:16 -0600

    Style updates

--- app/assets/javascripts/components/notifications/cable.js
+++ app/assets/javascripts/components/notifications/cable.js
@@ -1,21 +1,26 @@
 import App from '../../cable';
 
-App.notifications = App.cable.subscriptions.create("NotificationsChannel", {
-  connected: function() {
+if(App.cable && App.cable.subscriptions) {
+  App.notifications = App.cable.subscriptions.create("NotificationsChannel", {
+    connected: function() {
 
-  },
+    },
 
-  disconnected: function() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected: function() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  received: function(data) {
-    let d = JSON.parse(data);
-    console.log("Recieved data",d);
-    if(App.notifications.listener) {
-      App.notifications.listener(d);
+    received: function(data) {
+      let d = JSON.parse(data);
+      console.log("Recieved data",d);
+      if(App.notifications.listener) {
+        App.notifications.listener(d);
+      }
     }
-  }
-});
+  });
+}
+else {
+  App.notifications = null;
+}
 
 export default App.notifications;
